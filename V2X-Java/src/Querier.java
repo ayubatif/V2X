6b3b84d51a635e951327d23e31f5f00a23540a99
Justin Arieltan <agriad1@yahoo.com>
2020-04-25 17:11:14 +0200

    1.2 tiny bit of work of querier

--- V2X-Java/src/Querier.java
+++ V2X-Java/src/Querier.java
@@ -1,4 +1,6 @@
 public class Querier {
     static final int PORT = 2020;
-    static final String CERTIFICATE_LOCATION = "";
+    static final String CERTIFICATE_LOCATION = "~/Desktop/Thesis/Certificate/";
+    static final String CA_CERTIFICATE_LOCATION = "~/Desktop/Thesis/Certificate/CA-certificate.crt";
+
 }
