1a546c9deb541487a6ee66b8661973a20b3bd1d2
astro <aatif@kth.se>
2020-05-16 20:26:29 +0200

    11.53 Fixed another script typo

--- V2X-Java/compile.sh
+++ V2X-Java/compile.sh
@@ -4,21 +4,21 @@ mvn clean compile assembly:single
 
 mkdir obuA-setup
 cp target/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar obuA-setup/
-java -cp target/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar v2x.PseudonymAuthority a
+java -cp obuA-setup/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar v2x.PseudonymAuthority a
 for file in CADNS/*; do cp "$file" "${obuA-setup/Authentication/}";done
 for file in Log/*; do cp "$file" "${obuA-setup/}";done
 
 mkdir obuX-setup
 cp target/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar obuX-setup/
-java -cp target/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar v2x.PseudonymAuthority x
+java -cp obuX-setup/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar v2x.PseudonymAuthority x
 for file in CADNS/*; do cp "$file" "${obuX-setup/Authentication/}";done
-for file in Log/*; do cp "$file" "${obuA-setup/}";done
+for file in Log/*; do cp "$file" "${obuX-setup/}";done
 
 mkdir obuN-setup
 cp target/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar obuN-setup/
-java -cp target/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar v2x.PseudonymAuthority n
+java -cp obuN-setup/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar v2x.PseudonymAuthority n
 for file in CADNS/*; do cp "$file" "${obuN-setup/Authentication/}";done
-for file in Log/*; do cp "$file" "${obuA-setup/}";done
+for file in Log/*; do cp "$file" "${obuN-setup/}";done
 
 exit
 
