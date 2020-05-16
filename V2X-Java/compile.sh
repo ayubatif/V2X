6f126d3180d07e0fbd60cb937955a929909cb782
Justin Arieltan <agriad1@yahoo.com>
2020-05-16 21:35:45 +0200

    11.7 bash script more stuff

--- V2X-Java/compile.sh
+++ V2X-Java/compile.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 # Mvn compile then copy auth and bash to target
-#mvn clean compile assembly:single
-
-whoami
+mvn clean compile assembly:single
 
 mkdir obuASetup
+mkdir obuASetup/Authentication/
 cp target/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar obuASetup/
 java -cp obuASetup/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar v2x.PseudonymAuthority a
-for file in CADNS/*; do cp "$file" "${obuASetup/Authentication/}";done
-for file in Log/*; do cp "$file" "${obuASetup/}";done
+for file in CADNS/*; do cp "$file" "obuASetup/Authentication/";done
+for file in Log/*; do cp "$file" "obuASetup/";done
 
 mkdir obuXSetup
+mkdir obuXSetup/Authentication/
 cp target/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar obuXSetup/
 java -cp obuXSetup/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar v2x.PseudonymAuthority x
-for file in CADNS/*; do cp "$file" "${obuXSetup/Authentication/}";done
-for file in Log/*; do cp "$file" "${obuXSetup/}";done
+for file in CADNS/*; do cp "$file" "obuXSetup/Authentication/";done
+for file in Log/*; do cp "$file" "obuXSetup/";done
 
 mkdir obuNSetup
+mkdir obuNSetup/Authentication/
 cp target/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar obuNSetup/
 java -cp obuNSetup/V2X-Java-1.0-SNAPSHOT-jar-with-dependencies.jar v2x.PseudonymAuthority n
-for file in CADNS/*; do cp "$file" "${obuNSetup/Authentication/}";done
-for file in Log/*; do cp "$file" "${obuNSetup/}";done
+for file in CADNS/*; do cp "$file" "obuNSetup/Authentication/";done
+for file in Log/*; do cp "$file" "obuNSetup/";done
 
 exit
 
